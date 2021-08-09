304e2d89e10b7cf69aaa66a4ecfa7857a65081df
Andrew Fellenz <andrewfellenz@gmail.com>
2021-08-09 15:38:18 -0500

    pushing solution

--- scripts/modules/User.js
+++ scripts/modules/User.js
@@ -1,19 +1,15 @@
+function User(name, gender, age, registered, req, userStatus) {
+	this.userName = name;
+	this.userGender = gender;
+	this.userAge = age;
+	this.userCode = this.userGender + this.userAge;
+	this.registered = false;
+	this.userReq = req;
+	this.userStatus = null;
 
+	return {
+		user: User,
+	};
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export { User };
